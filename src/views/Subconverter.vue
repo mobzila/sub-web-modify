81767f7add002b4a8879cba1654720b89044a0a4
mobzila <91730585+mobzila@users.noreply.github.com>
2025-07-17 00:37:41 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "自用均衡后端【vless reality+hy1+hy2】": "https://vespdovworwx.us-east-1.clawcloudrun.com",
+          "自用均衡后端【vless reality+hy1+hy2】": "https://ozzakbadwiax.us-east-1.clawcloudrun.com",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
@@ -347,7 +347,7 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
-          { value: "https://vespdovworwx.us-east-1.clawcloudrun.com" },
+          { value: "https://ozzakbadwiax.us-east-1.clawcloudrun.com" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
           { value: "https://sub.d1.mk" },
